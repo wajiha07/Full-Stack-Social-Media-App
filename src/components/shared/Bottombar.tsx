5de74fd8dc2bf28aa0c455d457114063cd1cf1d0
Wajiha <wajihawarka07@gmail.com>
2024-01-16 21:23:32 +0600

    first commit

---
+++ src/components/shared/Bottombar.tsx
@@ -0,0 +1,35 @@
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
+
+const Bottombar = () => {
+  const { pathname } = useLocation();
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`group-hover:invert-white ${
+                isActive && "invert-white"
+              }`}
+            />
+            {link.label}
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Bottombar;
